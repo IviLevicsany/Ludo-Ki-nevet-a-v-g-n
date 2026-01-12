0068565afff07381bad2d9c6c90fa9a5d9fdee9b
Creipy05 <99033830+Creipy05@users.noreply.github.com>
2026-01-12 10:42:03 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+record struct Player(string Name, int Position, int Pieces);
+
 namespace Ludo
 {
     internal class Program
@@ -34,9 +36,60 @@ namespace Ludo
         }
         public void Jatek()
         {
+            /*
             p1_nev = Console.ReadLine();
             while (p1_nev == null)
                 Console.WriteLine("Nem adtál meg nevet, próbáld újra: ");
+            */
+            
+            List<Player> players = new List<Player>();
+            while (true)
+            {
+                Console.Write("Mennyi játékossal szertnél játszani? ");
+                /*if (int.TryParse(Console.ReadLine(), out int numPlayers) && numPlayers >= 2 && numPlayers <= 4)
+                {
+                    for (int i = 0; i < numPlayers; i++)
+                    {
+                        Console.Write("Add meg a játékos nevét: ");
+                        string name = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(name) && !players.Any(p => p.Name == name))
+                        {
+                            players.Add(new Player(name, 0, 4));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Érvénytelen vagy foglalt név, próbáld újra.");
+                            i--; // Ismételje meg a ciklust a név újragenerálásához
+                        }
+                    }
+                    break; // Kilép a ciklusból, ha sikeresen hozzáadtuk a játékosokat
+                }
+                else
+                {
+                    Console.WriteLine("Kérlek adj meg egy érvényes számot 2 és 4 között.");
+                }*/
+
+                for (int i = 0; i < players.Count; i++)
+                {
+                    Console.Write("Add meg a játékos nevét: ");
+                    string name = Console.ReadLine();
+                    if (name != players[i].Name)
+                    {
+                        if (!string.IsNullOrWhiteSpace(name))
+                        {
+                            players.Add(new Player(name, 0, 4));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nem adtál meg nevet, próbáld újra.");
+                        }
+                    }
+                    else
+                        Console.WriteLine("Ez a név már foglalt");
+                }
+            }
+
+
             while (p1_babu == 0 || p2_babu == 0 || p3_babu == 0 || p4_babu == 0)
             {
                 while (p1_hely < palya && p2_hely < palya && p3_hely < palya && p4_hely < palya)
